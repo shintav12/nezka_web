3bed7e7ad60ea6df5dff0f4bfff937fc813e8a69
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2018-11-07 19:21:41 -0500

    fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -52,6 +52,29 @@
                 </ul>
             </div>
         </nav>
+        <div class="home-wrapper">
+            <div id="slider" class="owl-carousel owl-theme">
+                @foreach($sliders as $slider)
+                    <div class="">
+                        <img src="{{$slider->image}}" >
+                        <div class="home-wrapper">
+                            <div class="">
+                                <div class="row">
+                                    <div class="col-md-10 col-xs-12 col-xs-offset-1">
+                                        <div class="home-content button_slider">
+                                            <h1 class="white-text" style="letter-spacing: 5px;font-size:60px;margin:0px">{{$slider->title}}</h1>
+                                            <p class="white-text" style="font-size:25px">{{$slider->subtitle}}</p>
+                                            <a class="outline-btn" href="#portafolio"  style="color:white!important; border-color: white!important; font-weight: bolder;font-size: 15px">Nuestros proyectos</a>
+                                            <a class="white-btn" href="#quien_eres" style="font-weight: bolder; font-size: 15px" >¿Quién eres?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </header>
     @yield('content')
     <footer id="footer" class="sm-padding bg-dark">
